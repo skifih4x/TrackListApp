f7ffa7e5595290153e31e40fbbfeac5722e0d58c
artem <arte@kb.ri>
2022-03-22 19:42:38 +0300

    set new Value

--- TrackListApp/ViewController.swift
+++ TrackListApp/ViewController.swift
@@ -11,9 +11,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "Cell index \(indexPath.row)"
+        
+        return cell
+    }
+    
+    
+}
