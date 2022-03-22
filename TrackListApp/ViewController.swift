a5078fd160eba5f1d2eeb6c5edf73d71b16ee9df
artem <arte@kb.ri>
2022-03-22 18:50:09 +0300

    Initial Commit

---
+++ TrackListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackListApp
+//
+//  Created by Артем Орлов on 22.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
