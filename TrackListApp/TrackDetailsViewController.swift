ce3ea7496f3f3fd90887b4ff41a61fec5f4a6275
artem <arte@kb.ri>
2022-03-23 17:44:56 +0300

    add button in navigation

--- TrackListApp/TrackDetailsViewController.swift
+++ TrackListApp/TrackDetailsViewController.swift
@@ -1,4 +1,4 @@
-xc//
+//
 //  TrackDetailsViewController.swift
 //  TrackListApp
 //
@@ -8,22 +8,16 @@ xc//
 import UIKit
 
 class TrackDetailsViewController: UIViewController {
-
+    
+    @IBOutlet var coverImageView: UIImageView!
+    @IBOutlet var trackNameLabel: UILabel!
+    
+    var track: Track!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        coverImageView.image = UIImage(named: track.title)
+        trackNameLabel.text = track.title
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- TrackListApp/TrackListViewController.swift
+++ TrackListApp/TrackListViewController.swift
@@ -14,6 +14,7 @@ class TrackListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 80
+        navigationItem.leftBarButtonItem = editButtonItem
     }
 
     // MARK: - Table view data source
@@ -22,6 +23,18 @@ class TrackListViewController: UITableViewController {
         trackList.count
     }
 
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        .none
+    }
+    
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        false
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let currentTrack = trackList.remove(at: sourceIndexPath.row)
+        trackList.insert(currentTrack, at: destinationIndexPath.row)
+    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "trackID", for: indexPath)
@@ -43,7 +56,10 @@ class TrackListViewController: UITableViewController {
     // MARK: - Navigation
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
+        guard let detailsVC = segue.destination as? TrackDetailsViewController else {return}
+        guard let indexPath = tableView.indexPathForSelectedRow else {return}
+        let track = trackList[indexPath.row]
+        detailsVC.track = track
     }
     
 
